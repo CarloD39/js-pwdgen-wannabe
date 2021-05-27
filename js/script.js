41a7c76fef79e00086f6d0e4e620f5eb96cb7b48
CarloD39 <carlodurand@hotmail.it>
2021-05-27 16:32:40 +0200

    Password

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-// var firstNumber = prompt ('inserisci il tuo numero');
-// var secondNumber = prompt ('inserisci il secondo numero')
-
-// var result = parseInt(firstNumber) + parseInt(secondNumber)
-
-// document.getElementById('numbeResult').innerHTML = "il risultato è:" + result;
-
-
-// var prezzoIntero = prompt ('inserisci prezzo');
-// var scontoPrezzo = prompt ('sconto')
-
-// var sconto = (parseInt(prezzoIntero) * parseInt(scontoPrezzo)) / 100;
-
-// var result = parseInt(prezzoIntero) - sconto;
-
-// document.getElementById('prezzoScontato').innerHTML = "prezzo finale è:" + result ;
-
 
 var nameUser = prompt ('enter your name');
 var lastaNameUser = prompt ('and your last name');
