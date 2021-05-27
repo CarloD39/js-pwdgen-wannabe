4d96984d24ae8b0767ddc80baf32229f58199c9d
CarloD39 <carlodurand@hotmail.it>
2021-05-27 16:32:11 +0200

    password

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// var firstNumber = prompt ('inserisci il tuo numero');
+// var secondNumber = prompt ('inserisci il secondo numero')
+
+// var result = parseInt(firstNumber) + parseInt(secondNumber)
+
+// document.getElementById('numbeResult').innerHTML = "il risultato è:" + result;
+
+
+// var prezzoIntero = prompt ('inserisci prezzo');
+// var scontoPrezzo = prompt ('sconto')
+
+// var sconto = (parseInt(prezzoIntero) * parseInt(scontoPrezzo)) / 100;
+
+// var result = parseInt(prezzoIntero) - sconto;
+
+// document.getElementById('prezzoScontato').innerHTML = "prezzo finale è:" + result ;
+
+
+var nameUser = prompt ('enter your name');
+var lastaNameUser = prompt ('and your last name');
+var colorUser = prompt ('preferit color');
+var number = 21;
+
+var result = nameUser + lastaNameUser + colorUser + number;
+
+document.getElementById('userPassword').innerHTML = result;
